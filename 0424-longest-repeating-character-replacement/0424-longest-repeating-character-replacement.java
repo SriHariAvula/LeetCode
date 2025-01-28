f6eba59338a24d03b1808a067acac44033f58c01
SriHariAvula <146001119+SriHariAvula@users.noreply.github.com>
2025-01-28 00:49:52 -0600

    Time: 28 ms (33.68%), Space: 44.9 MB (61.4%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -2,16 +2,19 @@ class Solution {
     public int characterReplacement(String s, int k) {
         HashMap<Character, Integer> longestSub = new HashMap<>();
         int windowStart = 0, maxLength=0, maxRepeatCharacter=0;
-        for(int i=0; i<s.length(); i++){
-            char rc = s.charAt(i);
+        for(int windowEnd=0; windowEnd<s.length(); windowEnd++){
+            char rc = s.charAt(windowEnd);
             longestSub.put(rc, longestSub.getOrDefault(rc, 0)+1);
             maxRepeatCharacter = Math.max(maxRepeatCharacter, longestSub.get(rc));
-            if(i-windowStart+1 - maxRepeatCharacter>k){
+            while(windowEnd-windowStart+1 - maxRepeatCharacter>k){
                 char lc = s.charAt(windowStart);
                 longestSub.put(lc, longestSub.get(lc)-1);
+                if(longestSub.get(lc)==0){
+                    longestSub.remove(lc);
+                }
                 windowStart++;
             }
-            maxLength=Math.max(maxLength, i-windowStart+1);
+            maxLength=Math.max(maxLength, windowEnd-windowStart+1);
         }
         return maxLength;
     }
