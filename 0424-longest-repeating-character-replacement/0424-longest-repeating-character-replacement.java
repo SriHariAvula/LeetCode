ddf525e76b1d3454055f4e36f7715c98694d56be
SriHariAvula <146001119+SriHariAvula@users.noreply.github.com>
2025-02-10 19:31:27 -0600

    Time: 35 ms (23.19%), Space: 45.4 MB (12.05%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,20 +1,17 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        HashMap<Character, Integer> longestSub = new HashMap<>();
-        int windowStart = 0, maxLength=0, maxRepeatCharacter=0;
+        HashMap<Character, Integer> basket = new HashMap<>();
+        int maxCount=0, maxLength=0, windowStart=0;
         for(int windowEnd=0; windowEnd<s.length(); windowEnd++){
             char rc = s.charAt(windowEnd);
-            longestSub.put(rc, longestSub.getOrDefault(rc, 0)+1);
-            maxRepeatCharacter = Math.max(maxRepeatCharacter, longestSub.get(rc));
-            while(windowEnd-windowStart+1 - maxRepeatCharacter>k){
+            basket.put(rc, basket.getOrDefault(rc, 0)+1);
+            maxCount = Math.max(maxCount, basket.get(rc));
+            if(windowEnd-windowStart+1-maxCount>k){
                 char lc = s.charAt(windowStart);
-                longestSub.put(lc, longestSub.get(lc)-1);
-                if(longestSub.get(lc)==0){
-                    longestSub.remove(lc);
-                }
+                basket.put(lc, basket.get(lc)-1);
                 windowStart++;
             }
-            maxLength=Math.max(maxLength, windowEnd-windowStart+1);
+            maxLength = Math.max(maxLength, windowEnd-windowStart+1);
         }
         return maxLength;
     }
